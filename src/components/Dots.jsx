a17e66f8d8e0a7e2759410615494f257308bdfef
Ion Stici <ionsticidev@gmail.com>
2023-12-10 10:36:41 +0200

    Update

--- src/components/Dots.jsx
+++ src/components/Dots.jsx
@@ -1,7 +1,14 @@
 import styles from "./../styles/Dots.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Dots({ grid, gridSize }) {
+  const [guessedTiles, setGuessedTiles] = useState(
+    Array.from({ length: grid.length }, () => false)
+  );
+
+  const [tile1, setTile1] = useState(undefined);
+  const [tile2, setTile2] = useState(undefined);
+
   const gridStyles = {
     gridTemplateColumns: `repeat(${
       +gridSize === 4 ? "4, minmax(0, 118px)" : "6, minmax(0, 82px)"
@@ -12,10 +19,78 @@ function Dots({ grid, gridSize }) {
     gap: `${+gridSize === 4 ? "12.3px" : "9.1px"}`,
   };
 
+  function resetTiles() {
+    setTile1(undefined);
+    setTile2(undefined);
+  }
+
+  function handleDotClick({ target }) {
+    if (guessedTiles.every((tile) => tile)) return;
+    const tile = target.dataset.tile;
+
+    if (!tile1) {
+      setTile1(tile);
+      return;
+    }
+
+    if (!tile2) {
+      setTile2(tile);
+      return;
+    }
+  }
+
+  useEffect(() => {
+    if (tile1 && tile2 && grid[+tile1] !== grid[+tile2]) {
+      setTimeout(() => resetTiles(), 250);
+    }
+
+    if (tile1 && tile2 && grid[+tile1] === grid[+tile2]) {
+      setGuessedTiles((prev) => {
+        const arr = [...prev];
+        arr[+tile1] = true;
+        arr[+tile2] = true;
+        return arr;
+      });
+
+      resetTiles();
+    }
+  }, [tile1, tile2]);
+
+  const tileYellowStyles = {
+    backgroundColor: "#FDA214",
+  };
+
+  const tileGreyStyles = {
+    backgroundColor: "#BCCED9",
+  };
+
+  const opacityStyles = {
+    opacity: 1,
+  };
+
   return (
     <ul className={styles.ul} style={gridStyles}>
       {grid.map((dot, i) => {
-        return <li key={i}>{dot}</li>;
+        return (
+          <li
+            key={i}
+            data-tile={i}
+            onClick={handleDotClick}
+            style={
+              +tile1 === i || +tile2 === i
+                ? tileYellowStyles
+                : guessedTiles[i]
+                ? tileGreyStyles
+                : {}
+            }
+          >
+            <span style={+tile1 === i || +tile2 === i ? opacityStyles : {}}>
+              {+tile1 === i && dot}
+              {+tile2 === i && dot}
+              {guessedTiles[i] && dot}
+            </span>
+          </li>
+        );
       })}
     </ul>
   );

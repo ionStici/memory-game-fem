d2a894268926034fa2d7e36144fe8c3ac3009e30
Ion Stici <ionsticidev@gmail.com>
2023-12-12 21:08:46 +0200

    Update

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -5,25 +5,74 @@ import { useContext } from 'react';
 function Scores({ setShowScores }) {
   const { scores, setScores } = useContext(ScoresContext);
 
-  console.log(scores);
+  const data = scores.slice().reverse();
 
-  const clearHistory = () => {
-    setScores([]);
-  };
+  const clearHistory = () => setScores([]);
 
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        <div>
-          <button onClick={() => setShowScores(false)}>Back</button>
-          <button onClick={clearHistory}>Clear Scores</button>
+        <div className={styles.btns_wrapper}>
+          <button onClick={() => setShowScores(false)}>
+            <img src="/arrow-back.svg" alt="" />
+          </button>
+          <button onClick={clearHistory}>Clear</button>
         </div>
 
-        <div>
-          {scores.map((score, i) => {
-            return <div key={i}>{i}</div>;
+        <ul className={styles.ul}>
+          {data.map((score, i, arr) => {
+            return (
+              <li key={i}>
+                <div>
+                  <p>Game</p>
+                  <p>{arr.length - i}</p>
+                </div>
+
+                <div>
+                  <p>Theme</p>
+                  <p>{score.theme}</p>
+                </div>
+
+                <div>
+                  <p>Players</p>
+                  <p>{score.numberOfPlayers}</p>
+                </div>
+
+                <div>
+                  <p>Grid Size</p>
+                  <p>{score.gridSize}</p>
+                </div>
+
+                {+score.numberOfPlayers === 1 && (
+                  <>
+                    <div>
+                      <p>Time</p>
+                      <p>{score.time}</p>
+                    </div>
+
+                    <div>
+                      <p>Moves</p>
+                      <p>{score.moves}</p>
+                    </div>
+                  </>
+                )}
+
+                {+score.numberOfPlayers > 1 && (
+                  <>
+                    {score.score.map((score, i) => {
+                      return (
+                        <div key={i}>
+                          <p>P{i + 1}</p>
+                          <p>{score}</p>
+                        </div>
+                      );
+                    })}
+                  </>
+                )}
+              </li>
+            );
           })}
-        </div>
+        </ul>
       </div>
     </section>
   );
